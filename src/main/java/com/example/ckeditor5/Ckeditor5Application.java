b5718b91b1e1b32e9887122399049f88f53de7a0
geunhokinn <gghh1220@gmail.com>
2024-08-22 18:24:05 +0900

    Feat: 프로젝트 생성

---
+++ src/main/java/com/example/ckeditor5/Ckeditor5Application.java
@@ -0,0 +1,13 @@
+package com.example.ckeditor5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ckeditor5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ckeditor5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ckeditor5/Ckeditor5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ckeditor5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ckeditor5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
