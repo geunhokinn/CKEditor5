3facb38db3fd62399f802e49c738db252c884965
geunhokinn <gghh1220@gmail.com>
2024-08-24 16:48:30 +0900

    Feat: SaveController 작성

--- src/main/java/com/example/ckeditor5/controller/SaveController.java
+++ src/main/java/com/example/ckeditor5/controller/SaveController.java
@@ -1,7 +1,22 @@
 package com.example.ckeditor5.controller;
 
+import com.example.ckeditor5.dto.SaveDTO;
+import com.example.ckeditor5.service.ArticleService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class SaveController {
+
+    private final ArticleService articleService;
+
+    @PostMapping("/save")
+    public String saveLogic(SaveDTO saveDTO) {
+
+        articleService.saveArticle(saveDTO);
+
+        return "redirect:/";
+    }
 }
