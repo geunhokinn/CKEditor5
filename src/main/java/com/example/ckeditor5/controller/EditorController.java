c3e1781979d04e7417f1f881f4b59e2464c13084
geunhokinn <gghh1220@gmail.com>
2024-08-26 02:31:53 +0900

    Feat: EditorController 작성 - 게시글 수정 페이지 컨트롤러 작성

--- src/main/java/com/example/ckeditor5/controller/EditorController.java
+++ src/main/java/com/example/ckeditor5/controller/EditorController.java
@@ -1,13 +1,28 @@
 package com.example.ckeditor5.controller;
 
+import com.example.ckeditor5.service.ArticleService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
+@RequiredArgsConstructor
 public class EditorController {
+
+    private final ArticleService articleService;
 
     @GetMapping("/editor")
     public String editorPage() {
+
+        return "editor";
+    }
+
+    @GetMapping("/editor/{id}")
+    public String updatePage(@PathVariable Long id, Model model) {
+
+        model.addAttribute("Article", articleService.getArticle(id));
 
         return "editor";
     }
