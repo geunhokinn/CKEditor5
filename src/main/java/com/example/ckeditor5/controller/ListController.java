4b41e9171cbcf053cc441be10cbd73e6b2a04311
geunhokinn <gghh1220@gmail.com>
2024-08-25 16:45:53 +0900

    Feat: ListController 작성 - 게시물 리스트 조회 컨트롤러 작성

--- src/main/java/com/example/ckeditor5/controller/ListController.java
+++ src/main/java/com/example/ckeditor5/controller/ListController.java
@@ -1,13 +1,21 @@
 package com.example.ckeditor5.controller;
 
+import com.example.ckeditor5.service.ArticleService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class ListController {
 
+    private final ArticleService articleService;
+
     @GetMapping("/list")
-    public String listPage() {
+    public String listPage(Model model) {
+
+        model.addAttribute("ArticleList", articleService.getArticles());
 
         return "list";
     }
